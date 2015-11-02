d101b28eab82b793b90c4aeff7e4cbec4b643209
Yurii Banin <pr0z.kh@gmail.com>
2015-11-02 18:38:51 +0200

    Testing push from eclipse.

--- src/main/java/org/ttsaudiosaver/web/core/Test.java
+++ src/main/java/org/ttsaudiosaver/web/core/Test.java
@@ -5,5 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		Object test = null;
 		System.out.println(test + (String) test);
+		System.out.println("Adding changes to test ssh key from eclipse");
 	}
 }
